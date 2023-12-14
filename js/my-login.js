7d548ea237774af5b39b5c3b7c0aa9f922408c25
phucnguyen1334 <luuthiennguyet@gmail.com>
2023-12-15 00:15:50 +0700

    add captcha

--- js/my-login.js
+++ js/my-login.js
@@ -25,7 +25,7 @@ $(function() {
 	
 	// Thêm hàm gọi reCAPTCHA
 	function executeRecaptcha() {
-		grecaptcha.execute('6LcfnjEpAAAAALIue-cdtzewBBOeiw7j4ZWtANfM', { action: 'login' })
+		grecaptcha.execute(process.env.RECAPTCHA_SITE_KEY, { action: 'login' })
 		  .then(function(token) {
 			// Token này cần được gửi cùng với yêu cầu đăng nhập đến máy chủ của bạn
 			// Xử lý đăng nhập và kiểm tra token tại máy chủ
