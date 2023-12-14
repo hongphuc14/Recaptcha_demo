c2fdbe4c03348ac70f119df7c786fefa94815d2a
phucnguyen1334 <luuthiennguyet@gmail.com>
2023-12-15 00:04:48 +0700

    add captcha

--- js/my-login.js
+++ js/my-login.js
@@ -18,10 +18,24 @@
 'use strict';
 
 $(function() {
+	// Gọi reCAPTCHA ngay sau khi trang được tải
+	grecaptcha.ready(function() {
+		executeRecaptcha();
+	});
+	
+	// Thêm hàm gọi reCAPTCHA
+	function executeRecaptcha() {
+		grecaptcha.execute('6LcfnjEpAAAAALIue-cdtzewBBOeiw7j4ZWtANfM', { action: 'login' })
+		  .then(function(token) {
+			// Token này cần được gửi cùng với yêu cầu đăng nhập đến máy chủ của bạn
+			// Xử lý đăng nhập và kiểm tra token tại máy chủ
+			// Thêm mã xử lý đăng nhập của bạn ở đây
+			console.log("reCAPTCHA token:", token);
+		  });
+	}
 
-	// author badge :)
-	var author = '<div style="position: fixed;bottom: 0;right: 20px;background-color: #fff;box-shadow: 0 4px 8px rgba(0,0,0,.05);border-radius: 3px 3px 0 0;font-size: 12px;padding: 5px 10px;">By <a href="https://twitter.com/mhdnauvalazhar">@mhdnauvalazhar</a> &nbsp;&bull;&nbsp; <a href="https://www.buymeacoffee.com/mhdnauvalazhar">Buy me a Coffee</a></div>';
-	$("body").append(author);
+	// var author = '<div style="position: fixed;bottom: 0;right: 20px;background-color: #fff;box-shadow: 0 4px 8px rgba(0,0,0,.05);border-radius: 3px 3px 0 0;font-size: 12px;padding: 5px 10px;">By <a href="https://twitter.com/mhdnauvalazhar">@mhdnauvalazhar</a> &nbsp;&bull;&nbsp; <a href="https://www.buymeacoffee.com/mhdnauvalazhar">Buy me a Coffee</a></div>';
+	// $("body").append(author);
 
 	$("input[type='password'][data-eye]").each(function(i) {
 		var $this = $(this),
@@ -77,12 +91,19 @@ $(function() {
 		});
 	});
 
-	$(".my-login-validation").submit(function() {
+	$(".my-login-validation").submit(function(event) {
 		var form = $(this);
         if (form[0].checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         }
+		// Lấy giá trị của token reCAPTCHA từ trình duyệt
+		var recaptchaToken = grecaptcha.getResponse();
+
+		// Thêm mã xử lý đăng nhập của bạn tại đây, bao gồm cả token reCAPTCHA
+		// Gửi dữ liệu đăng nhập và token đến máy chủ để xác thực
+		// Xử lý kết quả trả về từ máy chủ
+		console.log("Form submitted with reCAPTCHA token:", recaptchaToken);
 		form.addClass('was-validated');
 	});
 });
